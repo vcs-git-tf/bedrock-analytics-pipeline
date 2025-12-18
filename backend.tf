e463da15bc37b2509a07f8e51724b81be23e6bc5
Sammy <vcs.git@thefactor.org>
2025-12-18 11:38:26 -0700

    Add Terraform backend configuration

---
+++ backend.tf
@@ -0,0 +1,13 @@
+# backend.tf
+ 
+terraform {
+  backend "s3" {
+    # These values must be provided via command line or environment variables
+    # bucket         = "bedrock-analytics-dev-terraform-state"
+    # key            = "terraform.tfstate"
+    # region         = "us-east-1"
+    # dynamodb_table = "bedrock-analytics-dev-terraform-locks"
+    # encrypt        = true
+  }
+}
+
