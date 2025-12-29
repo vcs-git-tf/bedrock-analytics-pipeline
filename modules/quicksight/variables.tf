daaa60f5fb275bf5d4d23329bdf3e382b142d372
Sammy <vcs.git@thefactor.org>
2025-12-29 16:39:32 -0700

    Terraform nuke and deploy

--- modules/quicksight/variables.tf
+++ modules/quicksight/variables.tf
@@ -64,7 +64,7 @@ variable "athena_table_name" {
 variable "quicksight_user" {
   description = "QuickSight user name for permissions (required for resource permissions)"
   type        = string
-  default     = "arn:aws:quicksight:us-east-1:194191748922:user/default/tfAdmin01"
+  default     = "user/default/tfAdmin01"
 
   # validation {
   #   condition     = var.quicksight_user != ""
