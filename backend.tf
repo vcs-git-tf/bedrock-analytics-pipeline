10effc8fef89e68d35f14797d27726f0231b26cc
Sammy <vcs.git@thefactor.org>
2025-12-29 08:41:04 -0700

    Terraform nukeand redeploy

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
     # These values must be provided via command line or environment variables
-    bucket         = "bedrock-analytics-dev-terraform-state"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
+    bucket = "bedrock-analytics-dev-terraform-state"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
     # dynamodb_table = "bedrock-analytics-dev-terraform-locks"
-    encrypt        = true
+    encrypt = true
   }
 }
--- backend/backend.tfvars
+++
@@ -1,6 +0,0 @@
-bucket = "bedrock-analytics-dev-terraform-state"
-key    = "terraform.tfstate"
-region = "us-east-1"
-
-use_lockfile = true
-encrypt      = true
