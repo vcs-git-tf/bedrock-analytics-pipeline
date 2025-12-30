51ff145a6b927f370148b8156b814f022cfd3397
Sammy <vcs.git@thefactor.org>
2025-12-30 11:30:30 -0700

    Terraform nuke and deploy

--- modules/athena/main.tf
+++ modules/athena/main.tf
@@ -118,7 +118,7 @@ resource "aws_iam_role" "quicksight_athena_role" {
 # Comprehensive IAM policy for QuickSight-Athena integration
 resource "aws_iam_policy" "quicksight_athena_policy" {
   name        = "${var.project_name}-${var.environment}-quicksight-athena-policy"
-  description = "Comprehensive policy for QuickSight to access Quicksight, Athena, S3, and Glue"
+  description = "Comprehensive policy for Athena to access Quicksight, S3, and Glue"
 
   policy = jsonencode({
     Version = "2012-10-17"
