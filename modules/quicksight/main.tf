689f9001a3ee21342ed8df1979d388e5a2da416d
Sammy <vcs.git@thefactor.org>
2025-12-29 17:02:25 -0700

    Terraform nuke and deploy

--- modules/quicksight/main.tf
+++ modules/quicksight/main.tf
@@ -3,7 +3,7 @@ data "aws_caller_identity" "current" {}
 data "aws_partition" "current" {}
 data "aws_region" "current" {}
 data "aws_quicksight_user" "current" {
-  user_name = "user/default/tfAdmin01"
+  user_name = "tfAdmin01"
 }
 
 # Local values for consistent naming and configuration
--- modules/quicksight/variables.tf
+++ modules/quicksight/variables.tf
@@ -64,7 +64,7 @@ variable "athena_table_name" {
 variable "quicksight_user" {
   description = "QuickSight user name for permissions (required for resource permissions)"
   type        = string
-  default     = "user/default/tfAdmin01"
+  default     = "tfAdmin01"
 
   # validation {
   #   condition     = var.quicksight_user != ""
